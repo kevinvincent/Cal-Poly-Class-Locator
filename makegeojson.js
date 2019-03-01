5632b5fda2354051ced851a37215d5068e1da7ab
Kevin Vincent <kevinsundar@gmail.com>
2019-03-01 11:42:56 -0800

    Better

--- makegeojson.js
+++ makegeojson.js
@@ -5,16 +5,7 @@ var GeoJSON = require('geojson');
 var db = new Datastore({ filename: 'data.js', autoload: true });
 
 db.find({}, function (err, docs) {
-   var geojson = GeoJSON.parse(docs.map(x => {
-      return {
-         name: x.department.trim()+" "+x.courseNumber.trim()+"-"+x.sectionNumber.trim()+": "+x.courseName,
-         description: "**Building:** "+x.bldgName+"\n"+
-                      "**Room:** "+x.room+"\n"+
-                      "**Time:** "+x.time,
-         lat: x.lat,
-         lng: x.lng
-      }
-   }), {Point: ['lat', 'lng']});
+   var geojson = GeoJSON.parse(docs, {Point: ['lat', 'lng']});
    fs.writeFileSync('geo.json', JSON.stringify(geojson));
 });
  
