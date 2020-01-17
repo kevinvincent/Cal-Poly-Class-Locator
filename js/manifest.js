09745e79f306c3e1a70eb1f04510259485176154
root <root@ip-172-31-23-128.us-west-2.compute.internal>
2020-01-17 06:01:48 +0000

    Automatic Updates

--- js/manifest.js
+++ js/manifest.js
@@ -1,1 +1,1 @@
-var manifest = [{"name":"Fall 2019","dir":"fall2019","updated":1576537814449},{"name":"Winter 2020","dir":"winter2020","updated":1578636115433}]
+var manifest = [{"name":"Fall 2019","dir":"fall2019","updated":1576537814449},{"name":"Winter 2020","dir":"winter2020","updated":1579240908419}]
